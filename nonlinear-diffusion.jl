5b5c211d49e4a3bb8dd70136af293cc5e0cfdf75
Nicolau Leal Werneck <nwerneck@gmail.com>
2019-09-11 18:11:47 +0100

    refactor

--- nonlinear-diffusion.jl
+++ nonlinear-diffusion.jl
@@ -17,51 +17,17 @@ Lxx = imfilter(Lx, fx)
 Lxy = imfilter(Lx, fy)
 Lyy = imfilter(Ly, fy)
 
-# imshow(Lx)
-# imshow(Ly)
-# imshow(Lxx)
-# imshow(Lxy)
-# imshow(Lyy)
 
+function nld_step_scalar(Ld, c, stepsize)
+    Lstep = zeros(size(Ld))
 
+    dx = 0.5*stepsize*(c[1:end, 1:end-1] + c[1:end, 2:end]).*(Ld[1:end, 2:end] - Ld[1:end, 1:end-1])
+    dy = 0.5*stepsize*(c[1:end-1, 1:end] + c[2:end, 1:end]).*(Ld[2:end, 1:end] - Ld[1:end-1, 1:end])
 
-# function weickert_diffusivity(Lx, Ly, k)
-#     dst = zeros(size(Lx))
-#     invk2 = 1.0 / (k * k)
-#     weickert_diffusivity_calc(Lx, Ly, dst, invk2)
-#     dst
-# end
-
-# function weickert_diffusivity_calc(Lx, Ly, dst, invk2)
-#     sz = size(Lx)
-#     # @inbounds for y in 1:sz[1], x in 1:sz[2]
-#         # dst[y,x] = weickert_expression(Lx[y,x], Ly[y,x], invk2)
-#     # end
-#     @inbounds dst .= weickert_expression.(Lx, Ly, [invk2])
-# end
-
-# @inline function weickert_expression(lx, ly, invk2)
-#     dL = invk2 * (lx*lx + ly*ly)
-#     1.0 - exp(-3.315/(dL*dL*dL*dL))
-# end
-
-
-
-
-function calculate_diffusivity(func, Lx, Ly, k)
-    dst = zeros(size(Lx))
-    invk2 = 1.0 / (k * k)
-    nrow, ncol = size(Lx)
-    @inbounds begin
-        @simd for j in 1:nrow
-            @simd for k in 1:ncol
-                lx,ly = Lx[j,k],Ly[j,k]
-                dL = (lx*lx+ly*ly) * invk2
-                dst[j,k] = func(dL)
-            end
-        end
-    end
-    dst
+    Ld[1:end,1:end-1] .+= dx
+    Ld[1:end,2:end] .-= dx
+    Ld[1:end-1,1:end] .+= dy
+    Ld[2:end,1:end] .-= dy
 end
 
 pm_g1_diffusivity(Lx, Ly, k) = calculate_diffusivity(Lx, Ly, k) do dL
@@ -78,6 +44,22 @@ end
 
 charbonnier_diffusivity(Lx, Ly, k) = calculate_diffusivity(Lx, Ly, k) do dL
     1.0 / sqrt(1.0 + dL)
+end
+
+function calculate_diffusivity(func, Lx, Ly, k)
+    dst = zeros(size(Lx))
+    invk2 = 1.0 / (k * k)
+    nrow, ncol = size(Lx)
+    @inbounds begin
+        @simd for j in 1:nrow
+            @simd for k in 1:ncol
+                lx,ly = Lx[j,k],Ly[j,k]
+                dL = (lx*lx+ly*ly) * invk2
+                dst[j,k] = func(dL)
+            end
+        end
+    end
+    dst
 end
 
 function compute_k_percentile(Lx, Ly; nbins = 300, perc=0.2)
@@ -104,21 +86,6 @@ function compute_k_percentile(Lx, Ly; nbins = 300, perc=0.2)
     end
 end
 
-function nld_step_scalar(Ld, c, stepsize)
-    Lstep = zeros(size(Ld))
-
-    dx = 0.5*stepsize*(c[1:end, 1:end-1] + c[1:end, 2:end]).*(Ld[1:end, 2:end] - Ld[1:end, 1:end-1])
-    dy = 0.5*stepsize*(c[1:end-1, 1:end] + c[2:end, 1:end]).*(Ld[2:end, 1:end] - Ld[1:end-1, 1:end])
-
-    Ld[1:end,1:end-1] .+= dx
-    Ld[1:end,2:end] .-= dx
-    Ld[1:end-1,1:end] .+= dy
-    Ld[2:end,1:end] .-= dy
-end
-
-
-
-
 
 # imshow(Lsmooth)
 # aa = pm_g1_diffusivity(Lx, Ly, 0.01)
@@ -133,17 +100,18 @@ end
 
 # compute the k contrast
 
-gthresh = compute_k_percentile((@view Lx[2:end-1,2:end-1]), (@view Ly[2:end-1,2:end-1]); perc=0.95)
+# gthresh = compute_k_percentile((@view Lx[2:end-1,2:end-1]), (@view Ly[2:end-1,2:end-1]); perc=0.95)
 
 # imshow(sqrt.(Lx.^2+Ly.^2))
 # imshow(sqrt.(Lx.^2+Ly.^2) .< gthresh)
 
-Lflow = pm_g2_diffusivity(Lx, Ly, 0.01)
+# Lflow = pm_g2_diffusivity(Lx, Ly, 0.01)
+Lflow = charbonnier_diffusivity(Lx, Ly, 0.01)
 
 imshow(copy(Lt))
 for _ in 1:3
-    for _ in 1:99
-        nld_step_scalar(Lt, Lflow, 0.1)
+    for _ in 1:400
+        nld_step_scalar(Lt, Lflow, 0.05)
     end
     imshow(copy(Lt))
 end
