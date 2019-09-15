d4a714689e92588063d3c4d9b42127e2f6343799
Nicolau Leal Werneck <nwerneck@gmail.com>
2019-09-15 21:34:04 +0200

    first extracted points

--- akaze.jl
+++ akaze.jl
@@ -67,14 +67,16 @@ mylog2(i::Int, acc=0) = if i==1 acc else mylog2(i>>1,acc+1) end
 end
 
 @with_kw mutable struct KeyPoint
-    Point2f 	pt
-    float 	size
-    float 	angle = -1
-    float 	response = 0
-    int 	octave = 0
-    int 	class_id = -1
+    pt::Point
+    size::Float64
+    angle::Float64 = -1.0
+    response::Float64 = 0.0
+    octave::Int64 = 0
+    class_id::Int64 = -1
 end
 
+
+KeyPoint(a,b) = KeyPoint(a,b,-1.0,0.0,0,-1)
 
 ################################################################
 function Create_Nonlinear_Scale_Space(akaze, img)
@@ -137,15 +139,16 @@ function Feature_Detection(akaze) #::kpts
     Compute_Multiscale_Derivatives(akaze)
     Compute_Determinant_Hessian_Response(akaze)
     kpts = Find_Scale_Space_Extrema(akaze)
-    Do_Subpixel_Refinement(akaze, kpts)
+    # Do_Subpixel_Refinement(akaze, kpts)
 
     t2 = time_ns()
     akaze.timing_.detector = t2-t1
+    kpts
 end
 
 
 ################################################################
-function Compute_Multiscale_Derivatives()
+function Compute_Multiscale_Derivatives(akaze)
 
     t1 = time_ns()
 
@@ -171,11 +174,11 @@ function Compute_Determinant_Hessian_Response(akaze)
             @info "Computing detector response. Determinant of Hessian. Evolution time: $ev.etime"
         end
 
-        ev.Ldet .= ev.Lxx .* ex.Lyy - ev.Lxy.^2
+        ev.Ldet .= ev.Lxx .* ev.Lyy - ev.Lxy.^2
     end
 end
 
-#=
+
 ################################################################
 function Find_Scale_Space_Extrema(akaze)
 
@@ -183,7 +186,6 @@ function Find_Scale_Space_Extrema(akaze)
     dist = 0.0
     ratio = 0.0
     smax = 0.0
-    npoints = 0
     id_repeated = 0
     sigma_size_ = 0
     left_x = 0
@@ -195,133 +197,119 @@ function Find_Scale_Space_Extrema(akaze)
     is_repeated = false
     is_out = false
 
-    cv::KeyPoint point;
-    vector<cv::KeyPoint> kpts_aux;
+    point = KeyPoint(Point(0.0,0.0), 0)
+    kpts_aux = KeyPoint[]
+    kpts = KeyPoint[]
 
-  // Set maximum size
-  if (options_.descriptor == SURF_UPRIGHT || options_.descriptor == SURF ||
-      options_.descriptor == MLDB_UPRIGHT || options_.descriptor == MLDB) {
-    smax = 10.0*sqrtf(2.0f);
-  }
-  else if (options_.descriptor == MSURF_UPRIGHT || options_.descriptor == MSURF) {
-    smax = 12.0*sqrtf(2.0f);
-  }
+    ## Set maximum size
+    smax = if akaze.options_.descriptor in (SURF_UPRIGHT, SURF, MLDB_UPRIGHT, MLDB)
+        10.0*sqrt(2.0)
+    else  # (MSURF_UPRIGHT, MSURF)
+        12.0*sqrt(2.0)
+    end
 
-  t1 = time_ns()
+    t1 = time_ns()
 
-  for (size_t i = 0; i < evolution_.size(); i++) {
-    for (int ix = 1; ix < evolution_[i].Ldet.rows-1; ix++) {
+    for (i, ev) in enumerate(akaze.evolution_)
+        rows, cols = size(ev.Ldet)
 
-      float* ldet_m = evolution_[i].Ldet.ptr<float>(ix-1);
-      float* ldet = evolution_[i].Ldet.ptr<float>(ix);
-      float* ldet_p = evolution_[i].Ldet.ptr<float>(ix+1);
+        for (j, k) in Iterators.product(2:rows-1,2:cols-1)
 
-      for (int jx = 1; jx < evolution_[i].Ldet.cols-1; jx++) {
 
-        is_extremum = false;
+            is_extremum = false
+            is_repeated = false
+            is_out = false
+            value = ev.Ldet[j, k]
+
+            ## Filter the points with the detector threshold
+            if value > akaze.options_.dthreshold && value >= akaze.options_.min_dthreshold &&
+                value > ev.Ldet[j,k-1] && value > ev.Ldet[j,k+1] &&
+                value > ev.Ldet[j-1,k-1] && value > ev.Ldet[j-1,k] && value > ev.Ldet[j-1,k+1] &&
+                value > ev.Ldet[j+1,k-1] && value > ev.Ldet[j+1,k] && value > ev.Ldet[j+1,k+1]
+
+                is_extremum = true
+                point.response = abs(value)
+                point.size = ev.esigma*akaze.options_.derivative_factor
+                point.octave = ev.octave
+                point.class_id = i
+                ratio = 2.0^point.octave
+                sigma_size_ = round(Int64, point.size/ratio)
+                point.pt.x = k
+                point.pt.y = j
+
+                ## Compare response with the same and lower scale
+                for (pki, otherpoint) in enumerate(kpts_aux)
+
+                    if point.class_id-1 == otherpoint.class_id || point.class_id == otherpoint.class_id
+
+                        dist = ((point.pt.x*ratio-otherpoint.pt.x)*(point.pt.x*ratio-otherpoint.pt.x) +
+                                (point.pt.y*ratio-otherpoint.pt.y)*(point.pt.y*ratio-otherpoint.pt.y))
+
+                        if dist <= point.size*point.size
+                            if point.response > otherpoint.response
+                                id_repeated = pki
+                                is_repeated = true
+                            else
+                                is_extremum = false
+                            end
+                            break
+                        end
+                    end
+                end
+
+
+                ## Check out of bounds
+                if is_extremum
+                    ## Check that the point is under the image limits for the descriptor computation
+                    left_x = round(Int64, point.pt.x-smax*sigma_size_)-1
+                    right_x = round(Int64, point.pt.x+smax*sigma_size_) +1
+                    up_y = round(Int64, point.pt.y-smax*sigma_size_)-1
+                    down_y = round(Int64, point.pt.y+smax*sigma_size_)+1
+
+                    if left_x < 1 || right_x > cols || up_y < 1 || down_y > rows
+                        is_out = true
+                    end
+
+                    if is_out == false
+                        point.pt.x = point.pt.x*ratio + 0.5*(ratio-1.0)
+                        point.pt.y = point.pt.y*ratio + 0.5*(ratio-1.0)
+                        if is_repeated == false
+                            push!(kpts_aux, deepcopy(point))
+                        else
+                            kpts_aux[id_repeated] = deepcopy(point)
+                        end
+                    end ## if is_out
+                end ##if is_extremum
+            end
+        end ## for j, k
+    end ## for i
+
+    ## Now filter points with the upper scale level
+    for (i, point) in enumerate(kpts_aux)
+
         is_repeated = false;
-        is_out = false;
-        value = ldet[jx];
 
-        // Filter the points with the detector threshold
-        if (value > options_.dthreshold && value >= options_.min_dthreshold &&
-            value > ldet[jx-1] && value > ldet[jx+1] &&
-            value > ldet_m[jx-1] && value > ldet_m[jx] && value > ldet_m[jx+1] &&
-            value > ldet_p[jx-1] && value > ldet_p[jx] && value > ldet_p[jx+1]) {
+        for j in i+1:length(kpts_aux)
 
-          is_extremum = true;
-          point.response = fabs(value);
-          point.size = evolution_[i].esigma*options_.derivative_factor;
-          point.octave = evolution_[i].octave;
-          point.class_id = i;
-          ratio = pow(2.0f, point.octave);
-          sigma_size_ = fRound(point.size/ratio);
-          point.pt.x = jx;
-          point.pt.y = ix;
+            ## Compare response with the upper scale
+            if (point.class_id+1) == kpts_aux[j].class_id
 
-          // Compare response with the same and lower scale
-          for (size_t ik = 0; ik < kpts_aux.size(); ik++) {
+                dist = ((point.pt.x-kpts_aux[j].pt.x)*(point.pt.x-kpts_aux[j].pt.x) +
+                        (point.pt.y-kpts_aux[j].pt.y)*(point.pt.y-kpts_aux[j].pt.y))
 
-            if ((point.class_id-1) == kpts_aux[ik].class_id ||
-                point.class_id == kpts_aux[ik].class_id) {
+                if dist <= point.size*point.size && point.response < kpts_aux[j].response
+                    is_repeated = true
+                    break
+                end
+            end
+        end
 
-              dist = (point.pt.x*ratio-kpts_aux[ik].pt.x)*(point.pt.x*ratio-kpts_aux[ik].pt.x) +
-                     (point.pt.y*ratio-kpts_aux[ik].pt.y)*(point.pt.y*ratio-kpts_aux[ik].pt.y);
+        if is_repeated == false
+            push!(kpts, deepcopy(point))
+        end
+    end
 
-              if (dist <= point.size*point.size) {
-                if (point.response > kpts_aux[ik].response) {
-                  id_repeated = ik;
-                  is_repeated = true;
-                }
-                else {
-                  is_extremum = false;
-                }
-                break;
-              }
-            }
-          }
-
-          // Check out of bounds
-          if (is_extremum == true) {
-
-            // Check that the point is under the image limits for the descriptor computation
-            left_x = fRound(point.pt.x-smax*sigma_size_)-1;
-            right_x = fRound(point.pt.x+smax*sigma_size_) +1;
-            up_y = fRound(point.pt.y-smax*sigma_size_)-1;
-            down_y = fRound(point.pt.y+smax*sigma_size_)+1;
-
-            if (left_x < 0 || right_x >= evolution_[i].Ldet.cols ||
-                up_y < 0 || down_y >= evolution_[i].Ldet.rows) {
-              is_out = true;
-            }
-
-            if (is_out == false) {
-              if (is_repeated == false) {
-                point.pt.x = point.pt.x*ratio + .5*(ratio-1.0);
-                point.pt.y = point.pt.y*ratio + .5*(ratio-1.0);
-                kpts_aux.push_back(point);
-                npoints++;
-              }
-              else {
-                point.pt.x = point.pt.x*ratio + .5*(ratio-1.0);
-                point.pt.y = point.pt.y*ratio + .5*(ratio-1.0);
-                kpts_aux[id_repeated] = point;
-              }
-            } // if is_out
-          } //if is_extremum
-        }
-      } // for jx
-    } // for ix
-  } // for i
-
-  // Now filter points with the upper scale level
-  for (size_t i = 0; i < kpts_aux.size(); i++) {
-
-    is_repeated = false;
-    const cv::KeyPoint& point = kpts_aux[i];
-    for (size_t j = i+1; j < kpts_aux.size(); j++) {
-
-      // Compare response with the upper scale
-      if ((point.class_id+1) == kpts_aux[j].class_id) {
-
-        dist = (point.pt.x-kpts_aux[j].pt.x)*(point.pt.x-kpts_aux[j].pt.x) +
-            (point.pt.y-kpts_aux[j].pt.y)*(point.pt.y-kpts_aux[j].pt.y);
-
-        if (dist <= point.size*point.size) {
-          if (point.response < kpts_aux[j].response) {
-            is_repeated = true;
-            break;
-          }
-        }
-      }
-    }
-
-    if (is_repeated == false)
-      kpts.push_back(point);
-  }
-
-  t2 = time_ns()
-      akaze.timing_.extrema = t2-t1
-
+    t2 = time_ns()
+    akaze.timing_.extrema = t2-t1
+    kpts
 end
-=#
--- test.jl
+++ test.jl
@@ -1,3 +1,7 @@
+using Plots
+# pyplot()
+# plotly()
+
 using Images, TestImages
 using FileIO
 
@@ -20,13 +24,26 @@ akaze = AKAZE(opt)
 # evolution1.Compute_Descriptors(kpts1, desc1);
 
 Create_Nonlinear_Scale_Space(akaze, img)
+kpts = Feature_Detection(akaze)
 
 
-filenames = map(1:length(akaze.evolution_)) do n "akazetest-$(lpad(n,2,'0')).png" end
+mkcolorimage(img) = colorview(RGB, img, img, img)
 
-for (fn, x) in zip(filenames, akaze.evolution_)
-    save(fn, Gray.(x.Lt))
-end
 
-run(`montage -filter point -geometry 512x512 -tile 4x3 $filenames akazetest.png`)
-imshow(load("akazetest.png"))
+
+# function demo_scalespace()
+# filenames = map(1:length(akaze.evolution_)) do n "akazetest-$(lpad(n,2,'0')).png" end
+# for (fn, x) in zip(filenames, akaze.evolution_)
+    # save(fn, Gray.(x.Lt))
+# end
+# run(`montage -filter point -geometry 512x512 -tile 4x3 $filenames akazetest.png`)
+# imshow(load("akazetest.png"))
+# end
+
+
+pp = vcat([[p.pt.x p.pt.y] for p in kpts]...)
+
+save("pirate.png", testimage(imagename))
+
+# plot(mkcolorimage(testimage(imagename))[end:-1:1,:])
+# plot!(pp[:,1], 513-pp[:,2], m=3, l=0, color=:red, ratio=1)
